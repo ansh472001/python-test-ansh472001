c29e7be260efadc276fc2142c25a4b2d3488a0e1
Ansh vishwakarma <Anshvishwakarma93@gmail.com>
2024-05-09 13:10:46 +0530

    ansh vishwakarma

--- program1.py
+++ program1.py
@@ -1,6 +1,31 @@
 class Solution:
-   
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+    def getTotalIsles(self, grid):
+        if not grid:
+            return 0
+    
+        rows = len(grid)
+        cols = len(grid[0])
+        island_count = 0
+        
+
+        def flood_fill(row, col):
+
+            if 0 <= row < rows and 0 <= col < cols and grid[row][col] == "L":
+               
+                grid[row][col] = "W"
+               
+                flood_fill(row + 1, col)
+                flood_fill(row - 1, col)
+                flood_fill(row, col + 1)
+                flood_fill(row, col - 1)
+        
+        
+        for i in range(rows):
+            for j in range(cols):
+            
+                if grid[i][j] == "L":
+                    flood_fill(i, j)
+                    island_count += 1
+        
+        return island_count
+
